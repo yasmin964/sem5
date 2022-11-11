ef046b2336534410bd6793f941b4e048995ac63a
yasya <kalgexavva1970@icloud.com>
2022-11-11 21:20:05 +0300

    add new text

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,14 +1,29 @@
 ﻿//задайте массив,которое определяет есть ли это число в массиве
-int[] GetArray(int size,int minValue,int maxValue)
- {
-    int[] res=new int[size];
-    for (int i=0;i<size;i++)
-    { res[i]=new Random().Next(minValue,maxValue+1);
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
     }
     return res;
- 
- }
- void GetNumber(int A)
- {
 
- }
+}
+void FindNumber(int[] array, int N)
+{
+    for (int i = 0; i < array; i++)
+    {
+        if (array[i] == N)
+        {
+            Console.WriteLine("ДА");
+            return;
+
+        }
+
+    }
+    Console.WriteLine("НЕТ");
+}
+int[] array = GetArray(5, -9, 9);
+FindNumber(array,N);
+int N=int.Parse(Console.Readline()!);
+ Console.WriteLine(String.Join(" ",array));
