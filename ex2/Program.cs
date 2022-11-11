10c61600960646a7d2b4bc5f64511025ba2faf77
yasya <kalgexavva1970@icloud.com>
2022-11-11 21:29:18 +0300

    add new file

---
+++ ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿//напишите программу массива:положительные элементы замените на соответствующие отрицательные,и наоборот
+int[] GetArray(int size,int minValue,int maxValue)
+ {
+    int[] res=new int[size];
+    for (int i=0;i<size;i++)
+    { res[i]=new Random().Next(minValue,maxValue+1);
+    }
+    return res;
+ 
+ }
+ void Function (int[] array)
+ { 
+    for (int i = 0; i < array.Length;i++)
+    { 
+        array[i]*=-1;
+    }
+
+
+ };
+ int [] array=GetArray(4,-9,9);
+Console.WriteLine(String.Join(",",array));//используется для вывода массива
+Function(array);
+Console.WriteLine(String.Join(",",array));
