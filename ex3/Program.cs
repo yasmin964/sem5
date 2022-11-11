86876318f7bb7fab4e380f330328ada7913f78a8
yasya <kalgexavva1970@icloud.com>
2022-11-11 21:09:08 +0300

    add new text

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//задайте массив,которое определяет есть ли это число в массиве
+int[] GetArray(int size,int minValue,int maxValue)
+ {
+    int[] res=new int[size];
+    for (int i=0;i<size;i++)
+    { res[i]=new Random().Next(minValue,maxValue+1);
+    }
+    return res;
+ 
+ }
+ void GetNumber(int A)
+ {
+
+ }
