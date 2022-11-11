f13f3b8fc2ae34cbd9a171978bc75e9af66e84ae
yasya <kalgexavva1970@icloud.com>
2022-11-12 00:07:46 +0300

    add new text

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задайте массив вещественных чисел,найдите разницу между макс и мин элементом масссив
+void Differ(double [] array)
+{
+    double min = Int32.MaxValue;
+    double max = Int32.MinValue;
+    for(int i=0;i<array.Length;i++)
+    {
+        if (array[i]> max)
+        {
+            array[i]==max;
+        }
+        if (array[i] < min)
+        {
+            min =array[i];
+        }
+
+    }int di=max-min;
+    Console.WriteLine($"разница между мин и мах {di}");
+    
+}
+double [] array=Console.WriteLine("Введите ")
