5338eacbb35866ef7d50a107f1da48cd1996c514
yasya <kalgexavva1970@icloud.com>
2022-11-11 23:26:20 +0300

    add new text

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите массив,заполненнный случайными числами.найдите сумму чисел стоящих на нечетных позициях
+int[] GetArray(int size,int minValue,int maxValue)
+ {
+    int[] res=new int[size];
+    for (int i=0;i<size;i++)
+    { res[i]=new Random().Next(minValue,maxValue+1);
+    }
+    return res;
+ 
+ }
+void GetSum(int [] array)
+{
+    for (int i = 0; i < array.Length;i++)
+    {
+        if (i%2!=0)
+        {
+            int res=0;
+            int res= res+array[i];
+
+
+        }
+
+    }
+    Console.WriteLine($"сумма нечетных чисел {res}");
+    
+}
+int[] array=GetArray(10,-9,9);
+ Console.WriteLine(String.Join(" ",array));
+ CountNumbers(array);
